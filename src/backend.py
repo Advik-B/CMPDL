7a93fb09d3d355cc128907abb1d18b2381e7ebac
Advik <advik.b@gmail.com>
2022-01-13 15:07:43 +0530

    Update backend.py

--- src/backend.py
+++ src/backend.py
@@ -3,7 +3,8 @@ from logger import Logger
 from tree_generator import gentree
 from urllib.parse import unquote
 from clint.textui import progress
-from tkinter.ttk import Progressbar
+from PyQt5.QtWidgets import QProgressBar
+from time import perf_counter as now
 import zipfile
 import tempfile
 import os
@@ -13,18 +14,48 @@ import requests
 
 
 class ModPack:
+    def __init__(self, path: str, **kwargs) -> None:
+        if type(kwargs) == dict and len(kwargs) > 0:
+            self.log = kwargs.get("log_func")
+            self.secondry_log = kwargs.get("secondry_log")
+            # Set up the progress bar
+            self.progressbar = kwargs.get("pbar")
+            self.progressbar.setValue(0)
+            self.progressbar.setMaximum(100)
+            
+            self.sec_progressbar = QProgressBar(kwargs.get("pbar2"))
+            self.output_dir = kwargs.get("output_dir")
+            self.download_optional = kwargs.get("download_optional_mods")
+            self.keep_config = kwargs.get("keep_config")
+        
+        else:
+            logger = Logger()
+            self.log = logger.log
+            self.secondry_log = logger.log
+            
+            self.progressbar = QProgressBar()
+            self.sec_progressbar = self.progressbar
+            self.output_dir = "."
+        
+        self.ini = False
+        self.path = path
+        
+    @staticmethod
+    def step(pbar: QProgressBar, value: int):
+        pbar.setValue(pbar.value() + value)
 
-    def __init__(self, path) -> None:
-        pass
-
-
-if __name__ == '__main__':
-    test()
-
-# addon = curse.addon(238222)
-# print(addon.name)
-# # print(addon.description)
-# print(addon.authors[0].name)
-# print(addon.download_count)
-# file = addon.file(file_id=3569553)
-# print(file.download_url)
+    def init(self):
+        self.log("Initializing Curse client...", 'info')
+        start = now()
+        self.c = CurseClient()
+        stop = now()
+        self.log("Successfully initialized Curse client in %s seconds" % stop - start,
+                 'info')
+        del stop, start
+        self.log("Initializing ModPack...", 'info')
+        start = now()
+        if self.path.endswith('.zip'):
+            self.tempdir = tempfile.mkdtemp(prefix='CMPDL')
+            
+            
+        self.ini = True
