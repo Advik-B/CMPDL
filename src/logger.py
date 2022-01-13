7aec4a1daeef6eb1c61d6708fb96126c53a212d0
Advik <advik.b@gmail.com>
2022-01-13 12:40:24 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,24 @@
 # This is the logger module. (Not in pypi)
 
 import datetime
+import os
 from termcolor import colored
-
 
 class Logger:
     def __init__(self):
         date = datetime.datetime.now().strftime('%Y-%m-%d') + '.log'
-        self.log_file = 'logs/' + date
+        self.log_file_name = 'logs/' + date
+        if os.path.isfile('logs'):
+            try:
+                os.remove('logs')
+                os.mkdir('logs')
+            except [OSError, PermissionError]:
+                print("[ERROR] Unable to create logs directory")
+        elif os.path.isdir('logs'):
+            pass
+        else:
+            os.mkdir('logs')
+        self.log_file = open(self.log_file_name, 'a')
 
     def log(self, message, type_):
         # Get the current time with milliseconds
@@ -22,6 +33,12 @@ class Logger:
             "debug": 'cyan'
         }
         format_ = f'[{colored(time, "yellow")}]-\
-[{colored(type_.upper(), types[type_])}]:\
+[{colored(type_.upper(), types[type_])}]: \
 {colored(message, "white")}'
+        file_format = f'[{time}]-[{type_.upper()}]: {message}\n'
         print(format_)
+        self.log_file.write(file_format)
+        del format_, file_format
+        return f'[{time}]-[{type_.upper()}]: {message}'
+    def quit(self):
+        self.log_file.close()
