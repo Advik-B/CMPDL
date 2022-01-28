0ce365734bc2d6f8ab23b84a83179a901691aba1
Advik <advik.b@gmail.com>
2022-01-28 13:11:53 +0530

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,43 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-ext_modules = [
+import sys
+from cx_Freeze import setup, Executable
 
-    Extension("backend",  ["backend.py"]),
-    Extension("logger", ["logger.py"]),
-    Extension('tree_generator', ['tree_generator.py']),
-]
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {
+    "packages": [
+        "os",
+        'PyQt5',
+        'threading',
+        'sys',
+        'clint',
+        'termcolor',
+        'cursepy',
+        'requests'
+        ],
+    "excludes": [
+        
+        'tkinter',
+    ],
+    'include_files':[
+                    'assets/',
+                    'design.ui',
+            ],
+    "include_msvcr": True,
+    
+}
+
+base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
-    name = 'CMPDL',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules)
+    name = "CMPDL",
+    version = "2",
+    description = "Cuseforge Modpack DownLoader",
+    options = {"build_exe": build_exe_options,},
+    executables = [
+        Executable(
+            "__main__.py",
+            base=base,
+            icon='assets/icon.ico'
+            )
+        ]
+    )
