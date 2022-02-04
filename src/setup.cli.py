e20f5fc3a0eaca27a224b8fc860a9e9230f95f4a
Advik <advik.b@gmail.com>
2022-02-04 18:11:49 +0530

    Prep for #4  build
    rename `backend_cli.py` to `cli.py`
    create `setup.cli.py` for building
    
    Also: TYSM @juraj-hrivnak  for actually giving me the task ( I feel lonely sometimes )
    Co-Authored-By: Juraj Hrivnák <71150936+juraj-hrivnak@users.noreply.github.com>

---
+++ src/setup.cli.py
@@ -0,0 +1,45 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {
+    "packages": [
+        "os",
+        'cursepy',
+        'urllib',
+        'clint',
+        'time',
+        'zipfile',
+        'tempfile',
+        'json',
+        'shutil',
+        'requests',
+        'click',
+        'datetime',
+        'termcolor',
+            
+        ],
+    "excludes": [
+        
+        'tkinter',
+    ],
+
+    "include_msvcr": True,
+    
+}
+
+# base = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    name = "CMPDL",
+    version = "2.5",
+    description = "Cuseforge Modpack DownLoader",
+    options = {"build_exe": build_exe_options,},
+    executables = [
+        Executable(
+            "cli.py",
+            # base=base,
+            icon='assets/icon.ico'
+            )
+        ]
+    )
