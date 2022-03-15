f334d0979352c5b4a3d6d971128825b5f816c5ec
Advik <advik.b@gmail.com>
2022-03-15 12:36:38 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,41 +1,45 @@
 # This is the logger module. (Not in pypi)
-
+assert __name__ != "__main__", "This module should not be run directly. Import it instead."
 import datetime
 import os
 from termcolor import colored
+import colorama
 
 class Logger:
     def __init__(self):
-        date = datetime.datetime.now().strftime('%Y-%m-%d') + '.log'
-        self.log_file_name = 'logs/' + date
-        if os.path.isfile('logs'):
+        colorama.init()
+        date = datetime.datetime.now().strftime("%Y-%m-%d") + ".log"
+        self.log_file_name = "logs/" + date
+        if os.path.isfile("logs"):
             try:
-                os.remove('logs')
-                os.mkdir('logs')
+                os.remove("logs")
+                os.mkdir("logs")
             except [OSError, PermissionError]:
                 print("[ERROR] Unable to create logs directory")
-        elif os.path.isdir('logs'):
+        elif os.path.isdir("logs"):
             pass
         else:
-            os.mkdir('logs')
-        self.log_file = open(self.log_file_name, 'a')
+            os.mkdir("logs")
+        self.log_file = open(self.log_file_name, "a")
+        self.setup()
+
+    # Override
+    def setup(self):
+        pass
+
+    def log2(self, message, type_):
+        pass
 
     def log(self, message, type_):
         # Get the current time with milliseconds
         # time = datetime.datetime.now().strftime('%H:%M:%S:%f')
-        time = datetime.datetime.now().strftime('%H:%M:%S')
+        time = datetime.datetime.now().strftime("%H:%M:%S")
         # format_ = f'[{time}]-[{type_.upper()}]: {message}'
-        types = {
-
-            "info": 'green',
-            "warning": 'yellow',
-            "error": 'red',
-            "debug": 'cyan'
-        }
+        types = {"info": "green", "warning": "yellow", "error": "red", "debug": "cyan"}
         format_ = f'[{colored(time, "yellow")}]-\
 [{colored(type_.upper(), types[type_])}]: \
 {colored(message, "white")}'
-        file_format = f'[{time}]-[{type_.upper()}]: {message}\n'
+        file_format = f"[{time}]-[{type_.upper()}]: {message}\n"
         try:
             print(format_)
         except AttributeError:
@@ -48,8 +52,23 @@ class Logger:
                     self.log_file.write(char)
                 except UnicodeEncodeError:
                     pass
-                
+
         del format_, file_format
-        return f'[{time}]-[{type_.upper()}]: {message}'
+        self.log2(message, type_)
+        return f"[{time}]-[{type_.upper()}]: {message}"
+
+    def info(self, message):
+        self.log(message, "info")
+    
+    def warning(self, message):
+        self.log(message, "warning")
+    
+    def error(self, message):
+        self.log(message, "error")
+    
+    def debug(self, message):
+        self.log(message, "debug")
+
     def quit(self):
         self.log_file.close()
+        colorama.deinit()
