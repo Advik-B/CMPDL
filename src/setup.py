6b50cbca27e0208c73baa74dc1a72bddb484d3b6
Advik <advik.b@gmail.com>
2022-05-04 09:39:10 +0530

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -6,38 +6,32 @@ from cx_Freeze import setup, Executable
 build_exe_options = {
     "packages": [
         "os",
-        'PyQt5',
-        'threading',
-        'sys',
-        'clint',
-        'termcolor',
-        'cursepy',
-        'requests'
-        ],
-    "excludes": [
-        
-        'tkinter',
+        "PyQt5",
+        "threading",
+        "sys",
+        "clint",
+        "termcolor",
+        "cursepy",
+        "requests",
     ],
-    'include_files':[
-                    'assets/',
-                    'design.ui',
-            ],
+    "excludes": [
+        "tkinter",
+    ],
+    "include_files": [
+        "assets/",
+        "design.ui",
+    ],
     "include_msvcr": True,
-    
 }
 
 base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
-    name = "CMPDL",
-    version = "2",
-    description = "Cuseforge Modpack DownLoader",
-    options = {"build_exe": build_exe_options,},
-    executables = [
-        Executable(
-            "__main__.py",
-            base=base,
-            icon='assets/icon.ico'
-            )
-        ]
-    )
+    name="CMPDL",
+    version="2",
+    description="Cuseforge Modpack DownLoader",
+    options={
+        "build_exe": build_exe_options,
+    },
+    executables=[Executable("__main__.py", base=base, icon="assets/icon.ico")],
+)
