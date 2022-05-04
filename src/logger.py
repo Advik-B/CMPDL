03210bd69b9717eb810546e063c245765a24acf8
Advik <advik.b@gmail.com>
2022-05-04 09:38:56 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -4,38 +4,33 @@ import datetime
 import os
 from termcolor import colored
 
+
 class Logger:
     def __init__(self):
-        date = datetime.datetime.now().strftime('%Y-%m-%d') + '.log'
-        self.log_file_name = 'logs/' + date
-        if os.path.isfile('logs'):
+        date = datetime.datetime.now().strftime("%Y-%m-%d") + ".log"
+        self.log_file_name = "logs/" + date
+        if os.path.isfile("logs"):
             try:
-                os.remove('logs')
-                os.mkdir('logs')
+                os.remove("logs")
+                os.mkdir("logs")
             except [OSError, PermissionError]:
                 print("[ERROR] Unable to create logs directory")
-        elif os.path.isdir('logs'):
+        elif os.path.isdir("logs"):
             pass
         else:
-            os.mkdir('logs')
-        self.log_file = open(self.log_file_name, 'a')
+            os.mkdir("logs")
+        self.log_file = open(self.log_file_name, "a")
 
     def log(self, message, type_):
         # Get the current time with milliseconds
         # time = datetime.datetime.now().strftime('%H:%M:%S:%f')
-        time = datetime.datetime.now().strftime('%H:%M:%S')
+        time = datetime.datetime.now().strftime("%H:%M:%S")
         # format_ = f'[{time}]-[{type_.upper()}]: {message}'
-        types = {
-
-            "info": 'green',
-            "warning": 'yellow',
-            "error": 'red',
-            "debug": 'cyan'
-        }
+        types = {"info": "green", "warning": "yellow", "error": "red", "debug": "cyan"}
         format_ = f'[{colored(time, "yellow")}]-\
 [{colored(type_.upper(), types[type_])}]: \
 {colored(message, "white")}'
-        file_format = f'[{time}]-[{type_.upper()}]: {message}\n'
+        file_format = f"[{time}]-[{type_.upper()}]: {message}\n"
         try:
             print(format_)
         except AttributeError:
@@ -48,8 +43,9 @@ class Logger:
                     self.log_file.write(char)
                 except UnicodeEncodeError:
                     pass
-                
+
         del format_, file_format
-        return f'[{time}]-[{type_.upper()}]: {message}'
+        return f"[{time}]-[{type_.upper()}]: {message}"
+
     def quit(self):
         self.log_file.close()
