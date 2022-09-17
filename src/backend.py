34e54a455dab3ac4475eec72e54bc4fa3233bae1
Advik <advik.b@gmail.com>
2022-09-17 18:36:45 +0530

    Update backend.py

--- src/backend.py
+++ src/backend.py
@@ -73,6 +73,9 @@ class ModPack:
         """Make a temporary directory and return the path to it"""
         return tempfile.mkdtemp(prefix="CMPDL.TEMPORARY.")
 
+    def opendtemp(self):
+        os.system("explorer " + self.tempdir)
+
     def _ZIP(self):
         self.tempdir = self.makedtemp()
         self.log(f"Extracting [bold green]ZIP[/] file [b]{self.filename}[/] to [b yellow]{self.tempdir}[/]")
@@ -80,21 +83,21 @@ class ModPack:
             zip_.extractall(self.tempdir)
 
         self.log(gentree(self.tempdir))
-        os.system("explorer " + self.tempdir) #TODO: Remove this in production
+        self.opendtemp() #TODO: Remove this in production
 
     def _DIR(self):
         self.tempdir = self.makedtemp()
         self.log(f"Copying [b green]DIR[/] [b]{self.path}[/] to [b yellow]{self.tempdir}[/]")
         O = shutil.copytree(self.path, self.tempdir)
         self.log(gentree(O))
-        os.system("explorer " + self.tempdir) #TODO: Remove this in production
+        self.opendtemp() #TODO: Remove this in production
 
     def _JSON(self):
         self.tempdir = self.makedtemp()
         self.log(f"Copying [b green]JSON[/] [b]{self.path}[/] to [b yellow]{self.tempdir}[/]")
         shutil.copy(self.path, self.tempdir)
         self.log(gentree(self.tempdir))
-        os.system("explorer " + self.tempdir) #TODO: Remove this in production
+        self.opendtemp() #TODO: Remove this in production
 
     def clean(self):
         if self.method == "ZIP" or self.method == "DIR":
