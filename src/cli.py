7337c068f36d40ae30db8a7fb60c237764346ca1
Advik <advik.b@gmail.com>
2022-09-30 20:45:43 +0530

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -11,7 +11,50 @@ from rich.progress import Progress
 from rich.traceback import install
 
 import click
+import random
+import time
 
 c = Console()
 install(console=c, extra_lines=5, show_locals=True, indent_guides=True)
 
+with Progress(transient=True) as progress:
+    overall = progress.add_task("[b green]Overall[/]-[cyan]Progress[/]", total=1)
+    per_mod = progress.add_task("[b yellow]Individual[/]-[cyan]Progress[/]", total=1)
+
+    class per_mod_bar(CompatableProgressBar):
+        def __init__(self) -> None:
+            super().__init__()
+            self._init_()
+
+        def setTotalValue(self, val):
+            super().setTotalValue(val)
+            progress.update(per_mod, total=val)
+
+        def step(self, val: int = 1):
+            super().step(val)
+            progress.update(per_mod, advance=val)
+
+    class overall_bar(CompatableProgressBar):
+        def __init__(self) -> None:
+            super().__init__()
+            self._init_()
+
+        def setTotalValue(self, val):
+            super().setTotalValue(val)
+            progress.update(overall, total=val)
+
+        def step(self, val: int = 1):
+            super().step(val)
+            progress.update(overall, advance=val)
+
+    m = ModPack(
+        "sample.manifest.zip",
+        console=c,
+        output_dir="output",
+        progress_bar_overall=per_mod_bar(),
+        progress_bar_current=overall_bar(),
+        download_optional_mods=True,
+        )
+    m.initilize()
+    m.install()
+    m.clean()
