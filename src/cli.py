57861610986d79bedd5c29db0f7367ca1cf6f4e0
Advik <advik.b@gmail.com>
2022-10-28 23:12:51 +0530

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -17,13 +17,6 @@ import time
 c = Console()
 install(console=c, show_locals=True, extra_lines=4)
 
-@click.command(context_settings={"help_option_names": ["-h", "--help"]}, no_args_is_help=True)
-@click.argument("path", type=click.Path(exists=True, file_okay=True, dir_okay=True))
-@click.argument("output_dir", type=click.Path(exists=True, file_okay=False, dir_okay=True))
-@click.option("-c", "--chunk-size", type=int, default=1024, help="Chunk size for downloads")
-@click.option("-d","--download-optional", is_flag=True, default=True, help="Download optional files")
-@click.option("--no-clean", is_flag=True, default=False, help="Don't delete temp directory after install")
-
 
 class ProgressBar(CompatableProgressBar):
     def __init__(self):
@@ -32,20 +25,25 @@ class ProgressBar(CompatableProgressBar):
 
 
     def setTotalValue(self, value: int):
-        self.bar.update(value)  # type: ignore
+        self.bar = alive_bar(total=value)
 
     def step(self):
-        self.bar()  # type: ignore
+        self.bar()
 
     def set(self, value: int):
         self.bar.update(value)  # type: ignore
 
+@click.command(context_settings={"help_option_names": ["-h", "--help"]}, no_args_is_help=True)
+@click.argument("path", type=click.Path(exists=True, file_okay=True, dir_okay=True))
+@click.argument("output_dir", type=click.Path(file_okay=False, dir_okay=True))
+@click.option("-c", "--chunk-size", type=int, default=1024, help="Chunk size for downloads")
+@click.option("-d","--download-optional", is_flag=True, default=True, help="Download optional files")
+@click.option("--no-clean", is_flag=True, default=False, help="Don't delete temp directory after install")
 def cli(path: str, chunk_size: int, output_dir: str, download_optional: bool, no_clean: bool):
     try:
-        with alive_bar() as bar:
-            _temp_bar = ProgressBar()
-            _temp_bar._init_()
-            modpack = ModPack(
+        _temp_bar = CompatableProgressBar()
+        _temp_bar._init_()
+        modpack = ModPack(
                 console=c,
                 download_optional_mods=download_optional,
                 keep_files=no_clean,
@@ -53,10 +51,13 @@ def cli(path: str, chunk_size: int, output_dir: str, download_optional: bool, no
                 path=path,
                 progress_bar_current=ProgressBar(),
                 progress_bar_overall=_temp_bar,
+                chunk_size=chunk_size,
             )
-            modpack.install()
-            if not no_clean:
-                modpack.clean()
+        modpack.initilize()
+        modpack.install()
+        if not no_clean:
+            modpack.clean()
+
     except ModPackError:
         c.print_exception()
         c.print("[red]An error occured, please report this to the developer[/red]")
@@ -68,5 +69,6 @@ def cli(path: str, chunk_size: int, output_dir: str, download_optional: bool, no
     except ModPackNotFoundError:
         c.print_exception()
         c.print("[red]An error occured, please report this to the developer[/red]")
+
 
 cli() # type: ignore
