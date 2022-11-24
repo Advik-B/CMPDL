04bbad08030006de04709fb0de77455aad21d419
Advik <advik.b@gmail.com>
2022-11-24 21:01:21 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,9 +2,6 @@ from cursepy import CurseClient
 from v import api_key
 
 c = CurseClient(api_key)
-game = c.game(432) # Minecraft
-addon = c.addon(400012) # Ex Nihilo: Sequentia
 
-for file in addon.files():  # type: ignore
-    print(file.download_url)
-
+addon = c.addon(552574)
+print(addon.name)
