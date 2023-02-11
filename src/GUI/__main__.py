3178d40d16702995374a1f1c30e46012f3f1ffb0
Advik <advik.b@gmail.com>
2023-02-11 08:38:00 +0530

    Update __main__.py

--- src/GUI/__main__.py
+++ src/GUI/__main__.py
@@ -1,17 +1,19 @@
-from PyQt6.uic import loadUi
-from PyQt6.QtWidgets import QApplication, QWidget
+from .backend import client
 
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        loadUi("GUI/form.ui", self)
-        self.setup_ui()
-
-    def setup_ui(self):
-        self.show()
-
-
-if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    app.exec()
+# from PyQt6.uic import loadUi
+# from PyQt6.QtWidgets import QApplication, QWidget
+#
+# class MainWindow(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         loadUi("GUI/form.ui", self)
+#         self.setup_ui()
+#
+#     def setup_ui(self):
+#         self.show()
+#
+#
+# if __name__ == "__main__":
+#     app = QApplication([])
+#     window = MainWindow()
+#     app.exec()
