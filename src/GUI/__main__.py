7cb7e97012ebaed1011ccabcebb1a21774289f5c
Advik <advik.b@gmail.com>
2023-04-03 14:03:01 +0530

    Update __main__.py

--- src/GUI/__main__.py
+++ src/GUI/__main__.py
@@ -3,6 +3,10 @@ __version__ = "v2.6.0"
 from PyQt6.QtWidgets import *
 from PyQt6.uic import loadUi
 from sys import argv
+from qt_material import apply_stylesheet, list_themes
+from darkdetect import isDark
+from random import choice as random_choice
+
 from .v import API_KEY
 
 class CMPDL(QWidget):
@@ -13,6 +17,14 @@ class CMPDL(QWidget):
 
     def setupUI(self):
         loadUi("resources/main.ui", self)
+
+        dark_themes = [theme for theme in list_themes() if "dark" in theme and "red" not in theme]
+        light_themes = [theme for theme in list_themes() if "dark" not in theme and "red" not in theme]
+
+        if isDark():
+            apply_stylesheet(self, random_choice(dark_themes))
+        else:
+            apply_stylesheet(self, random_choice(light_themes))
 
 
 
