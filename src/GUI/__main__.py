9157a9c7937b35f136bbbf075ff05a309ed20e7c
Advik <advik.b@gmail.com>
2023-04-03 13:47:38 +0530

    Update __main__.py

--- src/GUI/__main__.py
+++ src/GUI/__main__.py
@@ -1,1 +1,21 @@
-from .backend import *
+__version__ = "v2.6.0"
+
+from PyQt6.QtWidgets import *
+from PyQt6.uic import loadUi
+from sys import argv
+
+class CMPDL(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+        self.show()
+
+    def setupUI(self):
+        loadUi("resources/main.ui", self)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(argv)
+    window = CMPDL()
+    app.exec()
