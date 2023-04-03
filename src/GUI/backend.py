1a25398551376dc9006ba4f623347ec5d81f96ea
Advik <advik.b@gmail.com>
2023-04-03 13:04:49 +0530

    Implement the download thread

--- src/GUI/backend.py
+++ src/GUI/backend.py
@@ -1,8 +1,74 @@
 from curseforge import CurseClient
 from PyQt6.QtCore import QThread, pyqtSignal
-from PyQt6.QtWidgets import QWidget, QProgressBar, QListView
+from PyQt6.QtWidgets import QWidget, QProgressBar, QListView, QLabel
 from PyQt6.QtGui import QStandardItemModel, QStandardItem
 from typing import Union
+from requests import get
+from urllib3.exceptions import InsecureRequestWarning
+from warnings import simplefilter
+from os.path import join as path_join
+
+class ModDownloadThread(QThread):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+
+        self.parent = parent
+        self.API: CurseClient = parent.API
+        self.mod_id: int = parent.mod_id
+        self.modfile_id: int = parent.modfile_id
+
+        self.progress: QProgressBar = parent.progress_bar
+        self.progress.setValue(0)
+        self.save_path: str = parent.save_path
+        self.parent.mod_name: QLabel
+        self.chunk_size: int = 4096 if parent.chunk_size is None else parent.chunk_size
+
+
+        # Conditions
+        self.parsed = False
+        self.downloaded = False
+
+        # Mod info
+        self.file_path: str = ""
+        self.download_url: str = ""
+        self.file_size: int = 0
+
+
+    def parse(self):
+        if self.parsed:
+            return
+
+        # Set the progress bar to be indeterminate
+        self.progress.setRange(0, 0)
+        mod = self.API.get_mod_file(self.mod_id, self.modfile_id)
+        self.file_path = path_join(self.save_path, mod.file_name)
+        self.download_url = mod.download_url
+        self.file_size = mod.file_length
+
+        self.parent.mod_name.setText(mod.display_name)
+
+        # Set the progress bar to be determinate
+        self.progress.setRange(0, self.file_size)
+        self.parsed = True
+
+    def download(self):
+        if self.downloaded:
+            return
+
+        self.parse()
+        self.progress.setMaximum(self.file_size)
+        with open(self.file_path, "wb") as f:
+            response = get(self.download_url, stream=True)
+            total = 0
+            for data in response.iter_content(chunk_size=self.chunk_size):
+                total += len(data)
+                f.write(data)
+                self.progress.setValue(total)
+        self.downloaded = True
+
+    run = download # Alias for download
+
+
 
 
 class ModDownloadList(QListView):
