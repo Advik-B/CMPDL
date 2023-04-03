42493ad5f1911ff93211e999c18ba7c05dcc0b64
Advik <advik.b@gmail.com>
2023-04-03 13:49:01 +0530

    Update v.py

--- src/GUI/v.py
+++ src/GUI/v.py
@@ -3,4 +3,4 @@ import base64
 # Yes, I know this is a bad idea.
 # I'm trusting the community to not abuse this.
 
-api_key = base64.b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh")
+API_KEY = base64.b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh")
