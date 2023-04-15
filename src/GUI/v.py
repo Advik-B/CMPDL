705b64bf4fe7e6f1e9190e649a0f264e4f97f90d
Advik <advik.b@gmail.com>
2023-04-15 11:23:32 +0530

    Update v.py

--- src/GUI/v.py
+++ src/GUI/v.py
@@ -4,3 +4,5 @@ import base64
 # I'm trusting the community to not abuse this.
 
 API_KEY = base64.b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh")
+# I used base64 to encode the API key, so that it's not visible to the code scanners to avoid detection.
+# You can decode it using base64.b64decode(). or by using an online decoder.
