f32e2467c21dec9adc3d9f1f1b7f3ce4ced5be37
Advik <advik.b@gmail.com>
2023-06-14 13:40:10 +0530

    Update extracter.py

--- src/GUI/backend/extracter.py
+++ src/GUI/backend/extracter.py
@@ -1,3 +1,17 @@
 import zipfile
 import json
 import os
+import shutil
+from curseforge import CurseClient
+from curseforge.classes import CurseModFileManifest
+from typing import Union
+
+from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtWidgets import QWidget, QProgressBar, QListView, QLabel
+from PyQt6.QtGui import QStandardItemModel, QStandardItem
+
+class Modpack:
+
+    def __init__(self,
+                 path: str,
+                 logger:
